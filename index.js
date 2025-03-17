7bd70fb82983da2edf75f8ab02ae9094cb599428
lutaii <ivanlutaienko@gmail.com>
2025-03-17 16:40:50 +0100

    Initial commit: Backend Setup & add lobby logic

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+
+const lobbies = {}
+
+function generateLobbyCode(length = 4) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    let result = ''
+    for (let i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() + chars.length))
+    }
+    return result
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello from Tic Tac Toe Backend!')
+})
+
+app.post('/lobby', (req, res) => {
+    let code = generateLobbyCode()
+
+    while (lobbies[code]) {
+        code = generateLobbyCode()
+    }
+
+    const uid = req.body.uid
+    if (!uid) {
+        return res.status(400).json({ error: 'Firebase UID is required' })
+    }
+
+    const lobby = {
+        code,
+        owner: uid,
+        players: [uid],
+        gameMode: 'classic',
+        status: 'waiting'
+    }
+
+    lobbies[code] = lobby
+    res.status(201).json({ code, lobby })
+})
+
+app.post('/lobby/:code/join', (req, res) => {
+    const code = req.params.code
+    const lobby = lobbies[code]
+
+    if (!lobby) {
+        return res.status(404).json({ error: 'Lobby not found' })
+    }
+
+    const player = req.body.player || 'Anonymous'
+
+    if (!lobby.players.includes(player)) {
+        lobby.players.push(player)
+    }
+
+    res.status(200).json({ message: `Joined lobby ${code}`, lobby })
+})
+
+app.get('/lobby/:code', (req, res) => {
+    const code = req.params.code
+    const lobby = lobbies[code]
+    if (!lobby) {
+        return res.status(404).json({ error: 'Lobby not found '})
+    }
+    res.json(lobby)
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
